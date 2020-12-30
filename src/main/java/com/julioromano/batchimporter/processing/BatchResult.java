ad6869fc8e532cd0759513e42389139b91b4ebb4
Julio Romano <julioromano@gmail.com>
2020-12-30 17:15:11 -0300

    Javadoc

--- src/main/java/com/julioromano/batchimporter/processing/BatchResult.java
+++ src/main/java/com/julioromano/batchimporter/processing/BatchResult.java
@@ -1,4 +1,15 @@
 package com.julioromano.batchimporter.processing;
 
+/**
+ * {@code BatchResult} is a superclass to represent the result
+ * of the process.
+ *
+ * Whenever a new Batch type is added to the system, a new subclass
+ * of {@code BatchResult} should be created with its
+ * required fields, representing the result of that specific process.
+ *
+ * @author  Julio Romano
+ * @since   1.0
+ */
 public abstract class BatchResult {
 }
